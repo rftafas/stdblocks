463af7af2e193c27c8d91aff6c5d4d8e0d0eb511
Ricardo Tafas <rftafas@repodinamica.com.br>
2020-11-11 17:40:54 -0300

    finished intra-process schedulling system.
    it is very unfortunate that Xilinx does not synthesizes generic on procedures.

--- hls_lib/vhls_lib.vhd
+++ hls_lib/vhls_lib.vhd
@@ -14,24 +14,46 @@
 ----------------------------------------------------------------------------------
 library IEEE;
   use IEEE.STD_LOGIC_1164.ALL;
+library expert;
+    use expert.std_string.all;
 
 package vhls_lib is
+  type procedure_status_t is ( BUSY, READY, PROC_DONE, PROC_ERROR);
+
+
+  constant max_proc_name : positive := 256;
+  constant max_proc_list : positive := 256;
+
+  type procedure_list_t is array (0 to max_proc_list-1) of string(1 to max_proc_name);
+  --type procedure_status_t is ( BUSY, READY, PROC_DONE, PROC_ERROR);
 
   type runner_t is record
-    program_counter : natural;
-    current_process : string(1 to 4);
+    current_process  : natural;
+    procedure_list   : procedure_list_t;
+    procedure_status : procedure_status_t;
+    total_procedures : natural;
   end record runner_t;
+  constant runner_start : runner_t := (
+    current_process  => 0,
+    procedure_list   => (others=>(others=>nul)),
+    procedure_status => READY,
+    total_procedures => 0
+  );
 
   procedure run
     generic (
-      process_name : string(1 to 4);
+      process_name : string;
       type          my_procedure_handler_t;
-      procedure     my_procedure(signal my_procedure_data : inout my_procedure_handler_t)
+      procedure     my_procedure(signal my_procedure_data : inout my_procedure_handler_t; my_status : out procedure_status_t)
     )
     parameter (
       runner : inout runner_t;
       signal my_procedure_data : inout my_procedure_handler_t
     );
+
+    procedure add_procedure(runner : inout runner_t; procname : in string);
+
+    procedure scheduler_procedure(runner : inout runner_t);
 
 end package;
 
@@ -39,20 +61,52 @@ package body vhls_lib is
 
   procedure run
     generic (
-      process_name : string(1 to 4);
+      process_name : string;
       type          my_procedure_handler_t;
-      procedure     my_procedure(signal my_procedure_data : inout my_procedure_handler_t)
+      procedure     my_procedure(signal my_procedure_data : inout my_procedure_handler_t; my_status : out procedure_status_t)
     )
     parameter (
       runner       : inout runner_t;
       signal my_procedure_data : inout my_procedure_handler_t
     ) is
+      variable proc_name_tmp : string(1 to max_proc_name) := (others => nul);
   begin
-    if runner.current_process = process_name then
-      my_procedure(my_procedure_data);
+    proc_name_tmp := string_padding(process_name,max_proc_name);
+    runner := runner;
+    add_procedure(runner,process_name);
+    if proc_name_tmp = runner.procedure_list(runner.current_process) then
+      my_procedure(my_procedure_data,runner.procedure_status);
     end if;
   end procedure;
 
+  procedure add_procedure(runner : inout runner_t; procname : in string) is
+    constant null_c : string(procname'range) := (others => nul);
+    variable proc_name_tmp : string(1 to max_proc_name) := (others => nul);
+  begin
+    proc_name_tmp := string_padding(procname,max_proc_name);
+    runner := runner;
+    for j in 0 to max_proc_list loop
+      if proc_name_tmp = runner.procedure_list(j) then
+        exit;
+      elsif runner.procedure_list(j) = null_c then
+        runner.procedure_list(j) := proc_name_tmp;
+        runner.total_procedures  := runner.total_procedures + 1;
+        exit;
+      end if;
+    end loop;
+  end add_procedure;
 
+  procedure scheduler_procedure(runner : inout runner_t) is
+  begin
+    runner := runner;
+    if runner.procedure_status /= BUSY then
+        runner.current_process := runner.current_process + 1;
+        if runner.current_process > runner.total_procedures then
+            runner.current_process := 0;
+        end if;
+    else
+      runner.procedure_status := READY;
+    end if;
+  end scheduler_procedure;
 
 end package body;
--- hls_lib/vhls_lib_tb.vhd
+++ hls_lib/vhls_lib_tb.vhd
@@ -23,39 +23,74 @@ end vhls_lib_tb;
 
 architecture behavioral of vhls_lib_tb is
 
-  type arit_t is record
-    input_1 : integer;
-    input_2 : integer;
-    result  : integer;
-  end record arit_t;
+--  --we have to define some static stuff for procedures to save.
+--  -- we use records ans inside them, store as many stuff as possible.
+ type arit_t is record
+   input_1 : integer;
+   input_2 : integer;
+   result  : integer;
+ end record arit_t;
 
-  procedure add_one ( signal add_io : inout arit_t) is
+--  --we have to declare a signal on the format of the handler.
+ signal artit_runner_s : arit_t := (
+   input_1 => 0,
+   input_2 => 0,
+   result  => 0
+ );
+
+--First, we write as many procedures we want.
+--We will have the status because we can do it several ways.
+procedure add_one ( signal add_io : inout arit_t; status : out procedure_status_t) is
+begin
+  add_io.result <= add_io.input_1 + 1;
+  status := PROC_DONE; --single cycle action.
+end add_one;
+
+  procedure add_two ( signal add_io : inout arit_t; status : out procedure_status_t) is
   begin
-    add_io.result <= add_io.input_1 + 1;
-  end add_one;
+    add_io.result <= add_io.input_1 + 2;
+    status := PROC_DONE; --single cycle action.
+  end add_two;
 
-  procedure run1 is new run
-    generic map (
-      process_name => "AAAA",
-      my_procedure_handler_t => arit_t,
-      my_procedure => add_one
-    );
+ procedure set_to_one ( signal add_io : inout arit_t; status : out procedure_status_t) is
+ begin
+   add_io.result <= 1;
+   status := PROC_DONE; --single cycle action.
+ end set_to_one;
 
-  signal artit_s : arit_t := (
-    input_1 => 0,
-    input_2 => 0,
-    result  => 0
-  );
+
+ --declare the runners. We must create one runner for each procedure.
+ procedure run1 is new run
+   generic map (
+     process_name => "NOME",
+     my_procedure_handler_t => arit_t,
+     my_procedure => add_one
+   );
+
+ procedure run2 is new run
+   generic map (
+     process_name => "NOM2",
+     my_procedure_handler_t => arit_t,
+     my_procedure => add_two
+   );
+
+ procedure run3 is new run
+   generic map (
+     process_name => "NOM3",
+     my_procedure_handler_t => arit_t,
+     my_procedure => set_to_one
+   );
 
 begin
 
+  --using a runner: will run according set sequence.
   process
-    variable runner_v : runner_t := (
-      program_counter => 0,
-      current_process => "AAAA"
-    );
+   variable runner_v : runner_t := runner_start;
   begin
-    run1(runner_v,artit_s);
+    scheduler_procedure(runner_v);
+    run1(runner_v,artit_runner_s);
+    run2(runner_v,artit_runner_s);
+    run3(runner_v,artit_runner_s);
     wait for 10 ns;
   end process;
 
